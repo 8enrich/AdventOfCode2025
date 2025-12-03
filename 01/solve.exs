10734f126469f0398117860d1f8afe6176889806
8enrich <henriqueftcunha@gmail.com>
2025-12-02 23:45:41 -0300

    Day 01

--- 01/solve.exs
+++ 01/solve.exs
@@ -29,31 +29,24 @@ defmodule Day01 do
   end
 
   def solve_02() do
-    Day01.get_parsed_input("./example.txt")
+    Day01.get_parsed_input("./input.txt")
     |> reduce(
       {0, 50}, fn x, { clicks, dial } -> 
-        rotate(clicks, dial, x)
+        next_dial = rem(rem(x, 100) + dial + 100, 100)
+        cond do
+          next_dial == 0 ->
+            { clicks + 1 + div(abs(x), 100), next_dial }
+          x < 0 and next_dial > dial and dial != 0 ->    
+            { clicks + 1 + div(abs(x), 100), next_dial }
+          x > 0 and next_dial < dial ->
+            { clicks + 1 + div(abs(x), 100), next_dial }
+          true ->
+            { clicks + div(abs(x), 100), next_dial }
+        end
       end
     )
     |> elem(0)
-  end
-  
-  def rotate(clicks, dial, direction) do
-    cond do
-      direction == 0 ->
-        { clicks, dial }
-      direction < 0 ->
-        { next_clicks, next_dial } = rotate(clicks, rem(dial - 1 + 100, 100), direction + 1) 
-        cond do
-          next_dial == 0 ->
-            { clicks + next_clicks, next_dial }
-        end
-        { clicks + next_clicks, next_dial }
-      direction > 0 ->
-        { next_clicks, next_dial } = rotate(clicks, rem(dial + 1 + 100, 100), direction - 1) 
-        { clicks + next_clicks, next_dial }
-    end
-  end
+  end 
 end
 
 IO.puts("01:\n" <> Integer.to_string(Day01.solve_01()))
